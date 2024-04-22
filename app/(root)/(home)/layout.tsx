47e3d8d83bfe8928fb549b4eccb05d2288109caa
Thusala Piyarisi <thusalapi@gmail.com>
2024-04-22 15:03:51 +0530

    fix bug

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,12 +1,14 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
 
 const HomeLayout = ({children} : {children: ReactNode}) => {
   return (
     <main className='relative'>
-        NavBar
+        <Navbar />
 
         <div className='flex'>
-            Sidebar
+            <Sidebar />
         </div>
 
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
@@ -15,7 +17,7 @@ const HomeLayout = ({children} : {children: ReactNode}) => {
             </div>
 
         </section>
-
+        
     </main>
   )
 }
