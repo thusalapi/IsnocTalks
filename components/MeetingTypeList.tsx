72263c46131b84ad013c3f3a24bf27d5890c04ca
Thusala Piyarisi <thusalapi@gmail.com>
2024-04-22 19:29:24 +0530

    Add createMeeting function to MeetingTypeList component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,11 +4,16 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation';
 import HomeCard from './HomeCard'
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined);
+
+    const createMeeting = () => {
+        
+        }
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
       <HomeCard
@@ -39,7 +44,17 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
       />
+
+        <MeetingModal
+          isOpen={meetingState === 'isInstantMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title ="Start an Instant Meeting"
+          className="text-center"
+          buttonText="Start Meeting"
+          handleClick={createMeeting}
+        />
     </section>
+
   )
 }
 
