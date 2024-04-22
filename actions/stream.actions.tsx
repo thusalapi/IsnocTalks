a005b7c869fe4f14e6509a9daa35b43045400920
Thusala Piyarisi <thusalapi@gmail.com>
2024-04-22 20:10:01 +0530

    Refactor StreamClientProvider and stream.actions.tsx

--- actions/stream.actions.tsx
+++ actions/stream.actions.tsx
@@ -1,24 +1,24 @@
-"user server"
+'use server';
 
-import { currentUser } from "@clerk/nextjs/server";
-import { StreamClient } from "@stream-io/node-sdk";
+import { currentUser } from '@clerk/nextjs/server';
+import { StreamClient } from '@stream-io/node-sdk';
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-const apiSecret = process.env.STREAM_API_SECRET;
+const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const STREAM_API_SECRET = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async () => {
-    const user = await currentUser();
-    if (!user) throw new Error('User not found');
-    if (!apiKey) throw new Error('Stream API key is missing');
-    if (!apiSecret) throw new Error('Stream API secret is missing');
+  const user = await currentUser();
 
-    const client = new StreamClient(apiKey, apiSecret);
+  if (!user) throw new Error('User is not authenticated');
+  if (!STREAM_API_KEY) throw new Error('Stream API key secret is missing');
+  if (!STREAM_API_SECRET) throw new Error('Stream API secret is missing');
 
-    const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
-    const issued = Math.floor(Date.now() / 1000) - 60;
+  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
 
-    const token = client.createToken(user.id, exp, issued);
+  const expirationTime = Math.floor(Date.now() / 1000) + 3600;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
 
-    return token;
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
 
+  return token;
 };
