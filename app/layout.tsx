d7f8306fb948828adce791c5f8e51d72b8965b3e
Thusala Piyarisi <thusalapi@gmail.com>
2024-05-13 02:15:55 +0530

    fix error and finalized

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,11 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
-  icons: {
-    icon: "/icons/logo.svg",
-  },
+  title: "IsnocTalks",
+  description: "Video calling and coaching app",
+  icons:{
+    icon: '/icons/logo.svg',
+  }
 };
 
 export default function RootLayout({
