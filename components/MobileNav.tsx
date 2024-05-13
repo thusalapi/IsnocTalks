de83c3b8b68268ee9a874b10e8e62bcce4d78a2c
Thusala Piyarisi <thusalapi@gmail.com>
2024-05-13 11:35:29 +0530

    refactor: update branding in MobileNav and Navbar components

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,7 +37,7 @@ const MobileNav = () => {
             className="max-sm:size-10"
             />
             <p className="text-[26px] font-extrabold text-white">
-            YOOM
+            IsnocTalks
             </p>
         </Link>
         <div className='flex h-[calc(100vh-72px)]
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = () => {
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="yoom logo"
+          alt="Isnoc logo"
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          YOOM
+          IsnocTalks
         </p>
       </Link>
 
