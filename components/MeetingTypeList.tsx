3443ca94b9a53659d64603e9439a9f1f3f45522d
Thusala Piyarisi <thusalapi@gmail.com>
2024-05-13 12:24:32 +0530

    refactor: update meeting type names and descriptions

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -73,28 +73,28 @@ const MeetingTypeList = () => {
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start an instant meeting"
+        title="New Session"
+        description="Start an instant session"
         handleClick={() => setMeetingState('isInstantMeeting')}
       />
       <HomeCard
         img="/icons/join-meeting.svg"
-        title="Join Meeting"
+        title="Join Session"
         description="via invitation link"
         className="bg-blue-1"
         handleClick={() => setMeetingState('isJoiningMeeting')}
       />
       <HomeCard
         img="/icons/schedule.svg"
-        title="Schedule Meeting"
-        description="Plan your meeting"
+        title="Schedule Coaching"
+        description="Plan your session"
         className="bg-purple-1"
         handleClick={() => setMeetingState('isScheduleMeeting')}
       />
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
-        description="Meeting Recordings"
+        description="Coaching Recordings"
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
       />
